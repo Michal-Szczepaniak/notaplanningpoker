4755c38e8e42728f48aaaee7fdf279e046e002dc
Michał Szczepaniak <m.szczepaniak.000@gmail.com>
2022-06-05 00:44:42 +0200

    Change server url to https

--- src/components/PokerGame.vue
+++ src/components/PokerGame.vue
@@ -37,7 +37,7 @@ export default {
     }
   },
   created() {
-    this.socket = io("http://server.napp.verdanditeam.com");
+    this.socket = io("https://server.napp.verdanditeam.com");
     this.socket.on("newPlayer", data => {
       console.log(data + " joined!");
       if (!this.players.find(el => el.name === data.name)) {
